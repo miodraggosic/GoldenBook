2e06b145fee0bf26984f934dc74b5578616cdb36
miodraggosic <gosmio88@gmail.com>
2022-11-15 14:21:47 +0100

    Book constructor added

--- components/Library.js
+++ components/Library.js
@@ -10,4 +10,13 @@ Library.prototype.addCategory = function () {};
 Library.prototype.filter = function () {};
 Library.prototype.render = function () {};
 
+function Book(url, title, author, year, category, description) {
+  this.imgUrl = url;
+  this.title = title;
+  this.author = author;
+  this.year = year;
+  this.category = category;
+  this.description = description;
+}
+
 export default Library;
