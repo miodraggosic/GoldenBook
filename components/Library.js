e9c0343b4cb4a8bfa269c54ec788235c35031144
miodraggosic <gosmio88@gmail.com>
2022-11-17 15:31:35 +0100

    regex added, valid field method added

--- components/Library.js
+++ components/Library.js
@@ -1,13 +1,19 @@
 function Library() {
   this.bookList = [];
   this.categories = ["general", "history", "fantasy"];
+  this.regex = {
+    imageUrl:
+      /^((http(s)?)?\:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/,
+    title: /^[A-Z]\w{10,50}$/,
+    author: /^[A-Z]\w{2,}(\s[A-Z]\w{2,})?$/,
+    year: /^(?:(?:18|19|20))([0-2]){2}$/,
+    description: /^[A-Z]\w{50,250}$/,
+  };
 }
 
 Library.prototype.getBooks = function (arr) {
   arr.forEach((elem, i) => {
     let num = Math.floor(i * Math.random());
-    console.log(num);
-
     let book = new Book(
       elem.website,
       elem.title,
@@ -19,6 +25,24 @@ Library.prototype.getBooks = function (arr) {
     );
     this.addBook(book);
   });
+};
+Library.prototype.validField = function (arr) {
+  function validate(field, regex) {
+    let result = regex.test(field.value);
+    if (result) {
+      field.classList.remove("red");
+      field.classList.add("green");
+    } else {
+      field.classList.add("red");
+      field.value = "";
+    }
+  }
+  arr.forEach((field) => validate(field, this.regex[field.name]));
+  let arrFields = Array.from(arr);
+
+  return arrFields.some((input) => input.classList.contains("red"))
+    ? false
+    : true;
 };
 
 Library.prototype.addBook = function (book) {
