5deda1be4cde8f13b4c4c390de266f415545b690
miodraggosic <gosmio88@gmail.com>
2022-11-17 19:28:04 +0100

    Library logic extracted to books.js

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 import { Users, User, Admin } from "./components/Users.js";
-import Library from "./components/Library.js";
-import getBooksDb from "./components/books.js";
 
 (function () {
   const inpEmail = document.getElementById("email");
@@ -23,24 +21,10 @@ import getBooksDb from "./components/books.js";
   let admin = new Admin("gosmio88@gmail.com", 132456789);
   console.log(admin);
 
-  //Books
-
-  const Api = "http://localhost:3000/books";
-  const booksDb = [];
-
-  let populateDb = () => getBooksDb(Api, booksDb);
-  populateDb();
-
-  let library = new Library();
-
   submitBtn.addEventListener("click", (e) => {
     e.preventDefault();
     let user = new User(inpEmail.value, inpPass.value);
     console.log(users.usersList);
-    console.log(booksDb);
-    library.getBooks(booksDb);
-
-    console.log(library.bookList);
 
     users.validUser(inputs)
       ? (users.addUser(user), updateDb(users.usersList), inputForm.submit())
