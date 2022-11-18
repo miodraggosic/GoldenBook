3d6ab7317a690d755646aed931b86135e15e0826
miodraggosic <gosmio88@gmail.com>
2022-11-18 10:32:23 +0100

    getBooksDB func modif, appendOpt add, authorField

--- components/books.js
+++ components/books.js
@@ -1,24 +1,50 @@
-import { Library } from "./Library.js";
-const addBookBtn = document.querySelector('button[value="add"]');
+import { Library, Book } from "./Library.js";
+(function () {
+  const addBookForm = document.querySelector(".addBook form");
+  const addBookBtn = document.querySelector('button[value="add"]');
+  const inputFields = document.querySelectorAll(".input[name]");
+  const authorField = document.querySelector("select[name='author']");
 
-console.log(addBookBtn);
+  const Api = "http://localhost:3000/books";
 
-const getBooksDb = (api, array) => {
-  fetch(api)
-    .then((response) => response.json())
-    .then((data) =>
-      data.forEach((elem) => {
-        array.push(elem);
-      })
-    );
-};
+  const getBooksDb = async () => {
+    const booksDb = await fetch(Api)
+      .then((response) => response.json())
+      .then((data) => data);
+    console.log(booksDb);
 
-//Books
+    let library = new Library();
+    library.getBooks(booksDb);
+    console.log(library.bookList);
 
-const Api = "http://localhost:3000/books";
-const booksDb = [];
+    const appendOptions = (name) => {
+      let elem = document.querySelector(`[name=${name}]`);
+      library.bookList.forEach((book) => {
+        let option = document.createElement("option");
+        option.textContent = `${book[name]}`;
+        elem.appendChild(option);
+      });
+    };
+    appendOptions("author");
+    appendOptions("category");
 
-let populateDb = () => getBooksDb(Api, booksDb);
-populateDb();
+    authorField.addEventListener("click", (e) => {
+      let descrpField = document.querySelector("textarea");
+      descrpField.attributes.placeholder.value = `${e.target.value} - description`;
+    });
 
-let library = new Library();
+    addBookBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      let inputValues = [];
+      inputFields.forEach((input) => inputValues.push(input.value));
+
+      let book = new Book(inputValues);
+
+      library.validField(inputFields)
+        ? (library.addBook(book), addBookForm.submit())
+        : library.errorMsg(inputFields);
+    });
+  };
+  getBooksDb();
+})();
